fe465c3468244e9f5d3f0cad7356a82619495c2d
lomuto42prisma <lomuto42prisma@gmail.com>
2023-11-21 20:55:49 +0900

    edit

--- pages/p/[id].tsx
+++ pages/p/[id].tsx
@@ -1,9 +1,9 @@
-import React from "react"
-import { GetServerSideProps } from "next"
-import ReactMarkdown from "react-markdown"
-import Layout from "../../components/Layout"
-import { PostProps } from "../../components/Post"
-import prisma from "../../lib/prisma"
+import React from "react";
+import { GetServerSideProps } from "next";
+import ReactMarkdown from "react-markdown";
+import Layout from "../../components/Layout";
+import { PostProps } from "../../components/Post";
+import prisma from "../../lib/prisma";
 
 
 
