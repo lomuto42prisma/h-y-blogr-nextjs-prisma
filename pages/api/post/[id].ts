633f26b6729b76e7c983250b1fd5de405851275d
lomuto42prisma <lomuto42prisma@gmail.com>
2023-11-24 10:19:01 +0900

    edit

--- pages/api/post/[id].ts
+++ pages/api/post/[id].ts
@@ -2,10 +2,11 @@
 
 // pages/api/post/[id].ts
 
+import { Post } from '@prisma/client';
 import prisma from '../../../lib/prisma';
 
 // DELETE /api/post/:id
-export default async function handle(req, res) {
+export default async function handle(req: { query: { id: any; }; method: string; }, res: { json: (arg0: Post) => void; }) {
   const postId = req.query.id;
   if (req.method === 'DELETE') {
     const post = await prisma.post.delete({
--- pages/api/publish/[id].ts
+++ pages/api/publish/[id].ts
@@ -1,10 +1,11 @@
 
 // pages/api/publish/[id].ts
 
+import { Post } from '@prisma/client';
 import prisma from '../../../lib/prisma';
 
 // PUT /api/publish/:id
-export default async function handle(req, res) {
+export default async function handle(req: { query: { id: any; }; }, res: { json: (arg0: Post) => void; }) {
   const postId = req.query.id;
   const post = await prisma.post.update({
     where: { id: postId },
