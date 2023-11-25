a17c36de5c7d5f60aae4e0729fa836dfe44a8020
lomuto42prisma <lomuto42prisma@gmail.com>
2023-11-25 10:29:54 +0900

    edit

--- pages/api/post/index.ts
+++ pages/api/post/index.ts
@@ -1,9 +1,10 @@
 
 
-// pages/api/post/index.ts
 
-import { getSession } from 'next-auth/react';
-import prisma from '../../../lib/prisma';
+
+import { getServerSession } from "next-auth/next";
+import prisma from "../../../lib/prisma";
+import { authOptions } from "../auth/[...nextauth]";
 
 // POST /api/post
 // Required fields in body: title
@@ -11,17 +12,39 @@ import prisma from '../../../lib/prisma';
 export default async function handle(req, res) {
   const { title, content } = req.body;
 
-  const session = await getSession({ req });
+  // console.log("req:", req);
+  // console.log("getSession", getSession);
+
+  const session = await getServerSession(req, res, authOptions);
+
+  console.log("the session:", session);
+
+  if (!session) {
+    res.status(401).json({ message: "You must be logged in to create a post." });
+    return;
+  }
+
+  const user = await prisma.user.findUnique({
+    where: {
+      email: session.user.email,
+    },
+  });
+
+  if (!user) {
+    res.status(404).json({ message: "User not found." });
+    return;
+  }
+
   const result = await prisma.post.create({
     data: {
       title: title,
       content: content,
-      author: { connect: { email: session?.user?.email } },
+      authorId: user.id,
     },
   });
+
   res.json(result);
 }
-
 
 
 
